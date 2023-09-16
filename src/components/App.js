3ccfaf7e36a94a82fbd2d4b453dfb29cd7a9e6ae
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-16 12:14:27 +0100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
-
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,26 +2,44 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import items from "../data/items";
 
-function ShoppingList({ items }) {
+function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = useState("");
+  const [shoppingItems, setShoppingItems] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSearchValue(event.target.value);
+  }
 
+  function handleItemFormSubmit(newItem) {
+    setShoppingItems([...shoppingItems, newItem]);
+  }
+
+  // Combine the filtered items into a single array
+  const filteredItems = shoppingItems.filter((item) => {
+    if (selectedCategory === "All") return true;
     return item.category === selectedCategory;
+  }).filter((item) => {
+    if (searchValue === "") return true;
+    return item.name.toLowerCase().includes(searchValue.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+        searchValue={searchValue}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
